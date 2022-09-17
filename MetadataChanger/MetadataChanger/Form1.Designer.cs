4de05f2c3bf6c7acf857c3a1e80de93760e17117
chauid <dlfdl28@naver.com>
2022-09-17 22:11:45 +0900

    upload

--- MetadataChanger/MetadataChanger/Form1.Designer.cs
+++ MetadataChanger/MetadataChanger/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace MetadataChanger
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button1);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
